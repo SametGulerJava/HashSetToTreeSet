107e57355bb101a07a9c8a09192f88a8a3836c35
Samet Güler <118828651+SametGulerJava@users.noreply.github.com>
2022-12-22 09:24:46 +0300

    Update Sets.java

--- Sets.java
+++ Sets.java
@@ -12,7 +12,11 @@ public class Sets {
     public static TreeSet cont() {
         String s = "Mississippi";
         String[] arr = s.split("");
+        
+        //HashSet used. Because TreeSets are very slow because of sorting all elements with natural order.
         HashSet<String> set = new HashSet<>(List.of(arr));
+        
+        //After adding the elements to "set" TreeSet going to sort all elements according to the natural order.
         TreeSet<String> trSet = new TreeSet<>(set);
         return trSet;
     }
